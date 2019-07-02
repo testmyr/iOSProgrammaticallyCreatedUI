cbcc16cb9e3fe6fe5ffd13fd821a4a71b1e376cd
Andrij Myronyuk <andrijmyronyuk@gmail.com>
2019-07-02 21:26:13 +0300

    Initial commit

---
+++ RedditSample/RedditSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditSample
+//
+//  Created by sdk on 7/2/19.
+//  Copyright © 2019 Sdk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
